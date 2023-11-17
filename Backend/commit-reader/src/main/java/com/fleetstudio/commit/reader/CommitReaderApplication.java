7bc37f9a8116bc305600c4ff9299ea1f5ecfeed9
selvabharathi <selvabharathi327@gmail.com>
2023-11-17 21:02:48 +0530

    initial project setup

---
+++ Backend/commit-reader/src/main/java/com/fleetstudio/commit/reader/CommitReaderApplication.java
@@ -0,0 +1,13 @@
+package com.fleetstudio.commit.reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommitReaderApplication.class, args);
+	}
+
+}
